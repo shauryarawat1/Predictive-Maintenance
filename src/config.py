6f728a5d46b0cb497dffd57528a9c76f95acbc66
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-09-17 17:09:35 -0400

    Set up the config file with prometheus server

---
+++ src/config.py
@@ -0,0 +1,8 @@
+# port on which prometheus server runs
+PROMETHEUS_PORT = 8000
+
+# URL to access the server
+PROMETHEUS_URL = f"http://localhost:{PROMETHEUS_PORT}"
+
+# interval to collect metrics
+COLLECTION_INTERVAL = 5
