7a0050d78c8ceeb3e8feeca6dde9ba6baaee7556
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-10-03 16:20:54 -0400

    Started test file for feature engineering

---
+++ tests/test_feature_engineering.py
@@ -0,0 +1,36 @@
+import pytest
+import pandas as pd
+import numpy as np
+from src.data_processing import engineer_features
+
+@pytest.fixture
+def sample_df():
+    dates = pd.date_range(start = '2021-01-01', end = '2021-01-02', freq = '5T')
+    
+    df = pd.DataFrame({
+        'cpu_usage_percent': np.random.rand(len(dates)) * 100,
+        'memory_usage_percent': np.random.rand(len(dates)) * 100,
+        'disk_usage_percent': np.random.rand(len(dates)) * 100,
+        'network_io_sent_bytes': np.random.rand(len(dates)) * 1e6,
+        'network_io_recv_bytes': np.random.rand(len(dates)) * 1e6,
+        'disk_io_read_bytes': np.random.rand(len(dates)) * 1e6,
+        'disk_io_write_bytes': np.random.rand(len(dates)) * 1e6
+    }, index = dates)
+    
+    return df
+
+def test_engineer_features(sample_df):
+    df_engineered = engineer_features(sample_df)
+    
+    # Check that new features are present
+    assert 'cpu_usage_percent_rolling_avg_5m' in df_engineered.columns
+    assert 'memory_usage_percent_rate_of_change' in df_engineered.columns
+    assert 'memory_cpu_ratio' in df_engineered.columns
+    assert 'hour_of_day' in df_engineered.columns
+    assert 'cpu_usage_percent_lag_5m' in df_engineered.columns
+    
+    # check if time based features are correct
+    assert (df_engineered['hour_of_day'] == df_engineered.index.hour).all()
+    assert (df_engineered['day_of_week'] == df_engineered.index.dayofweek).all()
+    
+    
